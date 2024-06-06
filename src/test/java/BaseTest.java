bdc116773f0f1e4ff42cf4187da63cfad56252b3
Pestrada72 <pearl.estrada@testpro.io>
2024-06-05 19:58:50 -0600

    Changes added some locators could not be made private as other classes were using them

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -7,54 +7,56 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Parameters;
-
+import org.testng.annotations.*;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.net.URI;
+import java.util.HashMap;
 
 public class BaseTest {
+    private static final ThreadLocal<WebDriver> THREAD_LOCAL = new ThreadLocal<>();
     protected static WebDriver driver;
     protected WebDriverWait wait;
     protected Actions actions;
-    public String url = "https://qa.koel.app/";
 
-    @BeforeSuite
-    static void setupClass() {
-
-        WebDriverManager.chromedriver().setup();
-        WebDriverManager.firefoxdriver().setup();
-        WebDriverManager.edgedriver().setup();
+    public static WebDriver getThreadLocal() {
+        return THREAD_LOCAL.get();
     }
 
     @BeforeMethod
-    @Parameters({"BaseURL", "browser"})
-    public void launchBrowser(String baseURL, String browser) throws MalformedURLException {
-        driver = pickBrowser(System.getProperty("browser"));
-
-        //Implicit Wait
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        actions = new Actions(driver);
-        navigateToPage(baseURL);
+    @Parameters({"BaseURL"})
+    public void setUpBrowser(@Optional String baseURL) throws MalformedURLException {
+        THREAD_LOCAL.set(pickBrowser(System.getProperty("browser")));
+        THREAD_LOCAL.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        THREAD_LOCAL.get().manage().window().maximize();
+        THREAD_LOCAL.get().manage().deleteAllCookies();
+        getThreadLocal().get(baseURL);
+        System.out.println("Browser setup by Thread " + Thread.currentThread().getId() + "and Driver reference is: " + getThreadLocal());
     }
 
-
-    @AfterMethod
-    public void closeBrowser() {
-        driver.quit();
-    }
-
-    public void navigateToPage(String baseURL) {
-        driver.get(baseURL);
+    public static WebDriver lambdaTest() throws MalformedURLException {
+        String hubURL = "https://hub.lambdatest.com/wd/hub";
+        String userName = "pearl.estrada";
+        String accessKey = "HMOuwBKL4COqEAwQcSpbzrqhsQEt0JIO0B3ArTOXzFDHd3EjkB";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("browserVersion", "125");
+        HashMap<String, Object> ltOptions = new HashMap<>();
+        ltOptions.put("username", userName);
+        ltOptions.put("accessKey", accessKey);
+        ltOptions.put("build", "Testing");
+        ltOptions.put("project", "Untitled");
+        ltOptions.put("w3c", true);
+        ltOptions.put("plugin", "java-java");
+        capabilities.setCapability("LT:Options", ltOptions);
+        return new RemoteWebDriver(new URL("https://" + userName + ":" + accessKey + hubURL), capabilities);
     }
 
     public static WebDriver pickBrowser(String browser) throws MalformedURLException {
@@ -63,13 +65,14 @@ public class BaseTest {
         switch (browser) {
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions optionsFirefox = new FirefoxOptions();
+                optionsFirefox.addArguments("-private");
                 return driver = new FirefoxDriver();
             case "MicrosoftEdge":
                 WebDriverManager.edgedriver().setup();
                 EdgeOptions edgeOptions = new EdgeOptions();
                 edgeOptions.addArguments("--remote-allow-origins=*");
                 return driver = new EdgeDriver(edgeOptions);
-
             //Grid Cases
             case "grid-edge":
                 caps.setCapability("browserName", "MicrosoftEdge");
@@ -77,6 +80,8 @@ public class BaseTest {
             case "grid-firefox":
                 caps.setCapability("browserName", "firefox");
                 return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "cloud":
+                return lambdaTest();
             default:
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions chromeOptions = new ChromeOptions();
@@ -85,9 +90,16 @@ public class BaseTest {
                 chromeOptions.addArguments("--incognito");
                 chromeOptions.addArguments("--start-maximized");
                 chromeOptions.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
-                return new RemoteWebDriver(URI.create(gridURL).toURL(), chromeOptions);
+                //return new RemoteWebDriver(URI.create(gridURL).toURL(), chromeOptions);
+                return driver = new ChromeDriver(chromeOptions);
 
         }
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        THREAD_LOCAL.get().close();
+        THREAD_LOCAL.remove();
     }
 
     public void provideEmail(String email) {
@@ -101,8 +113,11 @@ public class BaseTest {
         passwordField.clear();
         passwordField.sendKeys(password);
     }
+
     public void clickLoginBtn() {
         WebElement submit = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']")));
         submit.click();
     }
 }
+
+
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,56 +1,15 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pageObjects.AllSongsPage;
 import pageObjects.HomePage;
 import pageObjects.LoginPage;
+
 
 public class LoginTests extends BaseTest {
 
     @Test
-    public void loginValidEmailPassword() {
-        provideEmail("pearl.estrada@testpro.io");
-        providePassword("April969!!");
-        clickLoginBtn();
-        WebElement avatarIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img[class='avatar']")));
-        Assert.assertTrue(avatarIcon.isDisplayed());
-    }
-
-    @Test
-    public void loginInvalidEmailValidPassword() {
-        navigateToPage(url);
-        provideEmail("email@test.com");
-        providePassword("April969!!");
-        clickLoginBtn();
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-    }
-
-    @Test
-    public void loginValidEmailEmptyPassword() {
-        navigateToPage(url);
-        provideEmail("pearl.estrada@testpro.io");
-        providePassword(" ");
-        clickLoginBtn();
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-    }
-
-    @Test(dataProvider = "NegativeLoginTestData", dataProviderClass = TestDataProvider.class)
-    public void negativeLoginTest(String email, String password) {
-        provideEmail(email);
-        providePassword(password);
-        clickLoginBtn();
-        String expectedUrl = "https://qa.koel.app/";
-        Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
-        System.out.println("Just Testing Console");
-    }
-
-    @Test
     public void loginValidEmailValidPasswordTest() {
-        LoginPage loginPage = new LoginPage(driver);
-        HomePage homePage = new HomePage(driver);
-        AllSongsPage allSongsPage = new AllSongsPage(driver);
+        LoginPage loginPage = new LoginPage(getThreadLocal());
+        HomePage homePage = new HomePage(getThreadLocal());
 
         loginPage.provideEmail("pearl.estrada@testpro.io");
         loginPage.providePassword("April969!!");
@@ -60,9 +19,8 @@ public class LoginTests extends BaseTest {
 
     @Test
     public void loginValidEmailPasswordUsingPOM() {
-       LoginPage loginPage = new LoginPage(driver);
-       HomePage homePage = new HomePage(driver);
-       AllSongsPage allSongsPage = new AllSongsPage(driver);
+       LoginPage loginPage = new LoginPage(getThreadLocal());
+       HomePage homePage = new HomePage(getThreadLocal());
 
        loginPage.provideEmail("pearl.estrada@testpro.io");
        loginPage.providePassword("April969!!");
@@ -72,9 +30,8 @@ public class LoginTests extends BaseTest {
     }
     @Test
     public void loginValidEmailPasswordUsingPageFactory() {
-        LoginPage loginPage = new LoginPage(driver);
-        HomePage homePage = new HomePage(driver);
-        AllSongsPage allSongsPage = new AllSongsPage(driver);
+        LoginPage loginPage = new LoginPage(getThreadLocal());
+        HomePage homePage = new HomePage(getThreadLocal());
 
         loginPage.provideEmailToLogin("pearl.estrada@testpro.io");
         loginPage.providePasswordToLogin("April969!!");
