24c18562ca7b0756184b8608bbaa9ca857ba1a9a
Pestrada72 <pearl.estrada@testpro.io>
2024-06-10 01:25:04 -0600

    Homework26-completed

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 import java.net.MalformedURLException;
+
 import java.net.URL;
 import java.time.Duration;
 import java.net.URI;
