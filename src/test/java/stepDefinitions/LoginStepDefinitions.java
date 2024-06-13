40c8f2e42de0101fc3738a8ff89ef3617e9ef2a2
Pestrada72 <pearl.estrada@testpro.io>
2024-06-12 19:33:23 -0600

    Changes added some locators could not be made private as other classes were using them

---
+++ src/test/java/stepDefinitions/LoginStepDefinitions.java
@@ -0,0 +1,72 @@
+package stepDefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
+
+import java.time.Duration;
+
+public class LoginStepDefinitions {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @Before
+    public void openBrowser() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications");
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+}
+
+@Given("I open Login page")
+    public void openLoginPage() {
+        driver.get("https://qa.koel.app/");
+}
+
+@When("I enter email {string}")
+    public void i_enter_email(String email) {
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='email']"))).sendKeys(email);
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.provideEmail(email);
+    }
+
+@And("I enter password {string}")
+    public void i_enter_password(String password) {
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='password']"))).sendKeys(password);
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.providePassword(password);
+    }
+
+@And("I click submit")
+    public void clickSubmit() {
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[button='submit']"))).click();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.clickSubmit();
+    }
+
+@Then("I am logged in")
+    public void userIsLoggedIn() {
+        //Assert.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.avatar"))).isDisplayed());
+        HomePage homePage = new HomePage(driver);
+        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
+    }
+
+    @After
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+}
